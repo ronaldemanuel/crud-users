6e12c4d46075d080b70051a10a34d3cd7e30a565
ronaldemanuel <emronald15el@gmail.com>
2021-11-29 14:43:03 -0300

    fix: add username field on user update validator

--- app/Validators/User/UpdateValidator.ts
+++ app/Validators/User/UpdateValidator.ts
@@ -8,6 +8,7 @@ export default class UpdateValidator {
 
   public schema = schema.create({
     name: schema.string.optional({}, []),
+    username: schema.string.optional({}, [rules.maxLength(100)]),
     email: schema.string.optional({}, [
       rules.email(),
       rules.unique({ table: 'users', column: 'email' }),
@@ -15,6 +16,7 @@ export default class UpdateValidator {
     password: schema.string.optional({}, [rules.minLength(6)]),
   })
   public messages = {
+    'username.maxLength': 'O username deve ter no máximo 100 caracteres',
     'email.email': 'O email precisa ser válido',
     'email.unique': 'Este endereço de email já existe',
     'password.minLength': 'A senha precisa ter no minimo 6 caracteres',
