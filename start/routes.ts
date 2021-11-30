8823afc684f160f1e0347b16b7e3b44776918d31
ronaldemanuel <emronald15el@gmail.com>
2021-11-30 15:26:25 -0300

    feat: add auth middleware to users routes

--- start/routes.ts
+++ start/routes.ts
@@ -24,5 +24,7 @@ Route.get('/', async () => {
   return { hello: 'world' }
 })
 
-Route.resource('users', 'UsersController').apiOnly()
+Route.resource('users', 'UsersController')
+  .middleware({ index: ['auth'], show: ['auth'], update: ['auth'], destroy: ['auth'] })
+  .apiOnly()
 Route.post('/auth', 'AuthController.store')
