6f31d393ac0a2d79498c956e99936990070da5bc
ronaldemanuel <emronald15el@gmail.com>
2021-12-01 12:40:40 -0300

    feat: media migration

---
+++ database/migrations/1638369045774_medias.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Medias extends BaseSchema {
+  protected tableName = 'medias'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('file').notNullable()
+      table.string('type', 20)
+      table.string('subtype', 20)
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
