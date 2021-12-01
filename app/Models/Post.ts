80cb8c246570f4321ca97a2738a8e35ceb1f7e3f
ronaldemanuel <emronald15el@gmail.com>
2021-12-01 11:23:37 -0300

    feat: post model

---
+++ app/Models/Post.ts
@@ -0,0 +1,35 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export enum Visibility {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+  NOT_LISTED = 'not_listed',
+}
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @column()
+  public title: string
+
+  @column()
+  public content: string
+
+  @column()
+  public visibility: Visibility
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
